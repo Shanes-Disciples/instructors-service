bb1267cefad8398a18c2960cc756583bebe5eb55
Chris Zosa <zosachris@gmail.com>
2018-11-14 18:06:18 -0800

    build out CRUD routes

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.get('/instructors/:id', (req, res) => {
     .then(function getAllInstructors(data) {
       const info = [];
       const promises = [];
-
+    
       data.forEach(function getSingleInstructor(inst) {
         const instructor = ({
           id: inst.dataValues.inst_id,
@@ -31,6 +31,141 @@ app.get('/instructors/:id', (req, res) => {
 
           .then(function getCourseInfo(courses) {
             return mysql.Courses.findAll({
+              where: {
+                id: [courses
+                  .map(course => course.course_id)
+                  .filter(c => c != req.params.id)],
+              },
+            });
+          })
+
+          .then(function pushInfo(courseData) {
+            instructor.courseInfo = courseData;
+            info.push(instructor);
+          });
+
+        promises.push(newPromise);
+      });
+      return Promise.all(promises)
+        .then(() => res.send(info));
+    });
+});
+
+app.post('/instructors/:id', (req, res) => {
+  mysql.sequelize.authenticate()
+    .then(function addInstructorIds() {
+      return mysql.Join.create({ where: { course_id: req.params.id } });
+    })
+
+    .then(function addAllInstructors(data) {
+      const info = [];
+      const promises = [];
+    
+      data.forEach(function addSingleInstructor(inst) {
+        const instructor = ({
+          id: inst.dataValues.inst_id,
+          instInfo: null,
+          courseInfo: null,
+        });
+        const newPromise = mysql.Instructors.create({ where: { id: inst.dataValues.inst_id } })
+
+          .then(function addInstructorInfo(instData) {
+            instructor.instInfo = instData;
+            return mysql.Join.create({ where: { inst_id: inst.dataValues.inst_id } });
+          })
+
+          .then(function addCourseInfo(courses) {
+            return mysql.Courses.create({
+              where: {
+                id: [courses
+                  .map(course => course.course_id)
+                  .filter(c => c != req.params.id)],
+              },
+            });
+          })
+
+          .then(function pushInfo(courseData) {
+            instructor.courseInfo = courseData;
+            info.push(instructor);
+          });
+
+        promises.push(newPromise);
+      });
+      return Promise.all(promises)
+        .then(() => res.send(info));
+    });
+});
+
+app.put('/instructors/:id', (req, res) => {
+  mysql.sequelize.authenticate()
+    .then(function updateInstructorIds() {
+      return mysql.Join.update({ where: { course_id: req.params.id } });
+    })
+
+    .then(function updateAllInstructors(data) {
+      const info = [];
+      const promises = [];
+    
+      data.forEach(function updateSingleInstructor(inst) {
+        const instructor = ({
+          id: inst.dataValues.inst_id,
+          instInfo: null,
+          courseInfo: null,
+        });
+        const newPromise = mysql.Instructors.update({ where: { id: inst.dataValues.inst_id } })
+
+          .then(function updateInstructorInfo(instData) {
+            instructor.instInfo = instData;
+            return mysql.Join.update({ where: { inst_id: inst.dataValues.inst_id } });
+          })
+
+          .then(function updateCourseInfo(courses) {
+            return mysql.Courses.update({
+              where: {
+                id: [courses
+                  .map(course => course.course_id)
+                  .filter(c => c != req.params.id)],
+              },
+            });
+          })
+
+          .then(function pushInfo(courseData) {
+            instructor.courseInfo = courseData;
+            info.push(instructor);
+          });
+
+        promises.push(newPromise);
+      });
+      return Promise.all(promises)
+        .then(() => res.send(info));
+    });
+});
+
+app.delete('/instructors/:id', (req, res) => {
+  mysql.sequelize.authenticate()
+    .then(function deleteInstructorIds() {
+      return mysql.Join.destroy({ where: { course_id: req.params.id } });
+    })
+
+    .then(function deleteAllInstructors(data) {
+      const info = [];
+      const promises = [];
+    
+      data.forEach(function deleteSingleInstructor(inst) {
+        const instructor = ({
+          id: inst.dataValues.inst_id,
+          instInfo: null,
+          courseInfo: null,
+        });
+        const newPromise = mysql.Instructors.destroy({ where: { id: inst.dataValues.inst_id } })
+
+          .then(function deleteInstructorInfo(instData) {
+            instructor.instInfo = instData;
+            return mysql.Join.destroy({ where: { inst_id: inst.dataValues.inst_id } });
+          })
+
+          .then(function deleteCourseInfo(courses) {
+            return mysql.Courses.destroy({
               where: {
                 id: [courses
                   .map(course => course.course_id)
