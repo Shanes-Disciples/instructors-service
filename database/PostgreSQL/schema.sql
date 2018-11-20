d9d58e03fc40616dd12db46691163ccd3af7669b
Chris Zosa <zosachris@gmail.com>
2018-11-19 18:57:07 -0800

    adds schema for psql

---
+++ database/PostgreSQL/schema.sql
@@ -0,0 +1,45 @@
+DROP DATABASE udemy;
+
+CREATE DATABASE udemy;
+
+\C udemy;
+
+CREATE TABLE IF NOT EXISTS courses (
+  id SERIAL PRIMARY KEY,
+  course_name VARCHAR(255) NOT NULL,
+  rating DECIMAL(2, 1) NOT NULL,
+  reviews INT NOT NULL,
+  lectures INT NOT NULL,
+  num_hours INT NOT NULL,
+  full_price INT NOT NULL,
+  disc_price INT NOT NULL,
+  photo_url VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS instructors (
+  id SERIAL PRIMARY KEY,
+  inst_name VARCHAR(255) NOT NULL,
+  rating DECIMAL(2, 1) NOT NULL,
+  reviews INT NOT NULL,
+  students INT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  blurb VARCHAR(255) NOT NULL,
+  courses INT NOT NULL,
+  photo_url VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS joins (
+  id SERIAL PRIMARY KEY,
+  course_id INTEGER NOT NULL REFERENCES courses(id),
+  instructor_id INTEGER NOT NULL REFERENCES instructors(id)
+);
+
+
+\COPY courses (course_name, rating, reviews, lectures, num_hours, full_price, disc_price, photo_url) FROM './courseData.tsv' DELIMITER E'\t';
+\COPY instructors (inst_name, rating, reviews, students, title, blurb, courses, photo_url) FROM './instructorData.tsv' DELIMITER E'\t';
+\COPY joins (course_id, instructor_id) FROM './joinData.tsv' DELIMITER E'\t';
+
+
+
+-- postgres -D /usr/local/var/postgres (to start)
+-- psql postgres <database/PostgreSQL/schema.sql
